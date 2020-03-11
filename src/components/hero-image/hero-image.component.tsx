182eae255a27bf5e1c380a8a64bda4b4b4a79a66
Don Hamilton III <donhamiltoniii@gmail.com>
2020-03-11 00:28:44 -0400

    Update Hero Image

--- src/components/hero-image/hero-image.component.tsx
+++ src/components/hero-image/hero-image.component.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 import "./hero-image.styles.scss";
 
+import logo from "../../assets/images/PAS Logo/PAS_Blue_Web.png";
+
 const HeroImage = () => {
-  return <div className="hero-image"></div>;
+  return (
+    <div className="hero-image">
+      <figure className="hero-image__image-container">
+        <img alt="PAS logo" src={logo} />
+      </figure>
+    </div>
+  );
 };
 
 export default HeroImage;
