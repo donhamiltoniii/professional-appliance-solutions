19ae062a491d3d6adff0bbb79fad9e602dbb5253
Don Hamilton III <donhamiltoniii@gmail.com>
2024-09-03 10:53:45 -0400

    feat(gtm): update vite config
    - update vite config to initialize GTM connection
    - remove all other GTM related code

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
-import TagManager from "react-gtm-module";
-
-const tagManagerArgs = {
-  gtmId: import.meta.env.VITE_GOOGLE_TAG_MANAGER_ID,
-};
-
-TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
